66b0a7e60949f1bef01faf5e9c9d07c443b61828
JayKimCSM2019 <57769346+JayKimCSM2019@users.noreply.github.com>
2019-11-21 18:24:18 -0700

    Update week-12-utility.py

--- week-12-utility.py
+++ week-12-utility.py
@@ -3,31 +3,52 @@
 #Week 12
 
 def PrintOutput(string):
-    print(string)
+    print("OUTPUT",string)
 
 def Loadfile(file_name):
     file=open(file_name)
     lines=file.readlines()
     file.close()
     for line in lines:
-        words=lines.split('.')
-    print(words)
+        words=[].split(lines,'.')
+    print("OUTPUT",words)
     
 def UpdateString(string,letter,index):
-    word_list=string.split()
-    word_list[index]==letter
-    new_string=word_list.join()
-    print(new_string)
+    word_list=list(string)
+    word_list[index]=letter
+    new_string="".join(word_list)
+    print("OUTPUT",new_string)
 
 def FindWordCount(entered_list,entered_string):
     counter=0
     for i in entered_list:
         if i==entered_string:
             counter+=1
-    print(counter)
+    print("OUTPUT",counter)
 
 def ScoreBoard(players,scores,name):
+    new_list=[]
     for i in players:
+        new_list.append(i.lower())
         if i.lower()==name.lower():
             index=i.find(players)
-    print(name, "got a score of", scores[index])
+            print("OUTPUT",name, "got a score of", scores[index])
+        if name.lower() not in new_list:
+            print("OUTPUT","player not found")
+    
+def Union(list_1,list_2):
+    print("OUTPUT",list_1+list_2)
+
+def Intersection(list_1,list_2):
+    final_list=[]
+    for i in list_1:
+        if i in list_2:
+            final_list.append(i)
+    print("OUTPUT",final_list)
+
+def Notin(list_1,list_2):
+    final_list=[]
+    for i in list_1:
+        if i not in list_2:
+            final_list.append(i)
+    print("OUTPUT",final_list)
